30bc735ce5fa56f14550cd802684715a3a1ddf2c
Wolf-Dieter Klotz <wdklotz@gmail.com>
2016-03-02 13:16:46 +0100

    mods

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,4 +1,4 @@
-/* GET home page */
-module.exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+/* GET default home page */
+module.exports.index = function(req, res) {
+  res.render('index', { title: 'Express' }); // render view index.jade
 };
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,6 +3,6 @@ var router = express.Router();
 var ctrlMain = require('../controllers/main');
 
 /* GET home page. */
-router.get('/', ctrlMain.index);
+router.get('/', ctrlMain.index);  // index is default home
 
 module.exports = router;
