1325fa921985a529eeacd3e15ae810fe705f4b7b
Wolf-Dieter Klotz <wdklotz@gmail.com>
2016-03-04 09:19:37 +0100

    book pp 118

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,4 +1,16 @@
 /* GET about page */
 module.exports.about = function(req, res) {
-  res.render('generic_text', { title: 'About' }); // render view generic_text.jade
+  res.render('generic_text', { 
+	  title: 'About Loc8r',
+	  content: 'Loc8r was created to help people find places to sit down and get a bit of work done.\n\n \
+		  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem ac nisi dignissim accumsan. \
+		  Nullam sit amet interdum magna. Morbi quis faucibus nisi. Vestibulum mollis purus quis eros adipiscing \
+		  tristique. Proin posuere semper tellus, id placerat augue dapibus ornare. Aenean leo metus, tempus \
+		  in nisl eget accumsan interdum dui. Pellentesque sollicitudin volutpat ullamcorper.\n\n \
+		  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem ac nisi dignissim accumsan. \
+		  Nullam sit amet interdum magna. Morbi quis faucibus nisi. Vestibulum mollis purus quis eros adipiscing \
+		  tristique. Proin posuere semper tellus, id placerat augue dapibus ornare. Aenean leo metus, tempus \
+		  in nisl eget accumsan interdum dui. Pellentesque sollicitudin volutpat ullamcorper.'
+
+	  }); 
 };
