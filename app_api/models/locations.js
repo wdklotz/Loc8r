c8103d393c48fa28d384cc267b04202697922cd3
Wolf-Dieter Klotz <wdklotz@gmail.com>
2016-03-05 15:03:35 +0100

    book pp 200

--- app_api/models/locations.js
+++ app_api/models/locations.js
@@ -20,7 +20,7 @@ var locationSchema = new mongoose.Schema({
 	address: String,
 	rating: { type : Number, 'default' : 0, min : 0, max : 5},
 	facilities: [String],
-	coords: {type: [Number], index: '2dsphere'},
+	coords: {type: [Number], index: '2dsphere', rquired : true},
 	openingTimes: [openingTimeSchema],
 	reviews: [reviewSchema]
 });
@@ -28,79 +28,110 @@ var locationSchema = new mongoose.Schema({
 // compile the schema
 var Loc = mongoose.model('Location', locationSchema);
 
-// two document samples
-//var loc1 = new Loc({
-//name: 'Starcups',
-//address: '125 High Street, Reading, RG6 1PS',
-//rating: 3,
-//facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-//coords: [-0.9690884, 51.455041],
-//openingTimes: [{
-//	  days: 'Monday - Friday',
-//	  opening: '7:00am',
-//	  closing: '7:00pm',
-//	  closed: false
-//	  },{
-//	  days: 'Saturday',
-//	  opening: '8:00am',
-//	  closing: '5:00pm',
-//	  closed: false
-//	  },{
-//	  days: 'Sunday',
-//	  closed: true
-//	  }],
-//reviews: [{
-//	  author: 'Simon Holmes',
-//	  rating: 5,
-//	  timestamp: '16 July 2013',
-//	  reviewText: 'What a great place. I can\'t say enough good things about it.'
-//	  },{
-//	  author: 'Charlie Chaplin',
-//	  rating: 3,
-//	  timestamp: '16 June 2013',
-//	  reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
-//		  }]
-//}
-//);
-//
-//loc1.save(function(err){
-//if(err) return console.error(err);
-//});
+//three document samples: loc0 - loc2
+var loc0 = new Loc({
+name: 'Mac Donalds',
+address: '120 High Street, Reading, RG6 1PS',
+rating: 3,
+facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+coords: [-0.969088, 51.4550405],
+openingTimes: [{
+	  days: 'Monday - Friday',
+	  opening: '7:00am',
+	  closing: '7:00pm',
+	  closed: false
+	  },{
+	  days: 'Saturday',
+	  opening: '8:00am',
+	  closing: '5:00pm',
+	  closed: false
+	  },{
+	  days: 'Sunday',
+	  closed: true
+	  }],
+reviews: [{
+	  author: 'Christoph Rau',
+	  rating: 5,
+	  timestamp: '16 July 2013',
+	  reviewText: 'What a great place. I can\'t say enough good things about it.'
+	  },{
+	  author: 'Robert Klotz',
+	  rating: 3,
+	  timestamp: '16 June 2013',
+	  reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
+		  }]
+});
+var loc1 = new Loc({
+name: 'Starcups',
+address: '125 High Street, Reading, RG6 1PS',
+rating: 3,
+facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+coords: [-0.9690884, 51.455041],
+openingTimes: [{
+	  days: 'Monday - Friday',
+	  opening: '7:00am',
+	  closing: '7:00pm',
+	  closed: false
+	  },{
+	  days: 'Saturday',
+	  opening: '8:00am',
+	  closing: '5:00pm',
+	  closed: false
+	  },{
+	  days: 'Sunday',
+	  closed: true
+	  }],
+reviews: [{
+	  author: 'Simon Holmes',
+	  rating: 5,
+	  timestamp: '16 July 2013',
+	  reviewText: 'What a great place. I can\'t say enough good things about it.'
+	  },{
+	  author: 'Charlie Chaplin',
+	  rating: 3,
+	  timestamp: '16 June 2013',
+	  reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
+		  }]
+});
+var loc2 = new Loc({
+name: 'French Coffee Home',
+address: '27 Route du Château, Claix, F-38640',
+rating: 3,
+facilities: ['Hot drinks', 'Premium wifi'],
+coords: [5.6647765999, 45.1082854],
+openingTimes: [{
+	  days: 'Monday - Friday',
+	  opening: '7:00am',
+	  closing: '7:00pm',
+	  closed: false
+	  },{
+	  days: 'Saturday',
+	  opening: '8:00am',
+	  closing: '5:00pm',
+	  closed: false
+	  },{
+	  days: 'Sunday',
+	  closed: true
+	  }],
+reviews: [{
+	  author: 'Simon Holmes',
+	  rating: 5,
+	  timestamp: '16 July 2013',
+	  reviewText: 'What a great place. I can\'t say enough good things about it.'
+	  },{
+	  author: 'Charlie Chaplin',
+	  rating: 3,
+	  timestamp: '16 June 2013',
+	  reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
+		  }]
+});
 
-//var loc2 = new Loc({
-//name: 'French Coffee Home',
-//address: '27 Route du Château, Claix, F-38640',
-//rating: 3,
-//facilities: ['Hot drinks', 'Premium wifi'],
-//coords: [5.6647765999, 45.1082854],
-//openingTimes: [{
-//	  days: 'Monday - Friday',
-//	  opening: '7:00am',
-//	  closing: '7:00pm',
-//	  closed: false
-//	  },{
-//	  days: 'Saturday',
-//	  opening: '8:00am',
-//	  closing: '5:00pm',
-//	  closed: false
-//	  },{
-//	  days: 'Sunday',
-//	  closed: true
-//	  }],
-//reviews: [{
-//	  author: 'Simon Holmes',
-//	  rating: 5,
-//	  timestamp: '16 July 2013',
-//	  reviewText: 'What a great place. I can\'t say enough good things about it.'
-//	  },{
-//	  author: 'Charlie Chaplin',
-//	  rating: 3,
-//	  timestamp: '16 June 2013',
-//	  reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
-//		  }]
-//}
-//);
-//
+//loc0.save(function(err){
+//	if(err) return console.error(err);
+//});
+//loc1.save(function(err){
+//	if(err) return console.error(err);
+//});
 //loc2.save(function(err){
 //	if(err) return console.error(err);
 //});
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -7,14 +7,14 @@ var ctrlReviews = require('../controllers/reviews');
 // locations
 router["get"]   ('/locations',ctrlLocations.locationsListByDistance)
 router["post"]  ('/locations',ctrlLocations.locationsCreate);
-router["get"]   ('/locations/:locationid', ctrlLocations.locationsReadOne);
-router["put"]   ('/locations/:locationid', ctrlLocations.locationsUpdateOne);
-router["delete"]('/locations/:locationid', ctrlLocations.locationsDeleteOne);
+router["get"]   ('/location/:locationid', ctrlLocations.locationsReadOne);
+router["put"]   ('/location/:locationid', ctrlLocations.locationsUpdateOne);
+router["delete"]('/location/:locationid', ctrlLocations.locationsDeleteOne);
 
 // reviews
-router["post"]  ('/locations/:locationid/reviews', ctrlReviews.reviewsCreate);
-router["get"]   ('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsReadOne);
-router["put"]   ('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsUpdateOne);
-router["delete"]('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsDeleteOne);
+router["post"]  ('/location/:locationid/reviews', ctrlReviews.reviewsCreate);
+router["get"]   ('/location/:locationid/review/:reviewid', ctrlReviews.reviewsReadOne);
+router["put"]   ('/location/:locationid/review/:reviewid', ctrlReviews.reviewsUpdateOne);
+router["delete"]('/location/:locationid/review/:reviewid', ctrlReviews.reviewsDeleteOne);
 
 module.exports = router;
