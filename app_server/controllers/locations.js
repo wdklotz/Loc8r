2b0a2a294d5e4bdb125a5a7e8d2df9607698cee8
Wolf-Dieter Klotz <wdklotz@gmail.com>
2016-03-07 18:23:05 +0100

    book pp 263

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -11,17 +11,17 @@ if (process.env.NODE_ENV === 'production') {
 	apiOptions.server = "https://getting-mean-loc8r.herokuapp.com";
 }
 
-var _formatDistance = function (distance) {
-	var numDistance, unit;
-	if (distance > 1) {
-		numDistance = parseFloat(distance).toFixed(1);
-		unit = 'km';
-	} else {
-		numDistance = parseInt(distance * 1000,10);
-		unit = 'm';
-	}
-	return numDistance + unit;
-};
+//var _formatDistance = function (distance) {
+//	var numDistance, unit;
+//	if (distance > 1) {
+//		numDistance = parseFloat(distance).toFixed(1);
+//		unit = 'km';
+//	} else {
+//		numDistance = parseInt(distance * 1000,10);
+//		unit = 'm';
+//	}
+//	return numDistance + unit;
+//};
 
 var _showError = function (req, res, status) {
 	var title, content;
@@ -63,50 +63,21 @@ var getLocationInfo = function (req, res, callback) {
 
 //=====================================================	
 //------------------render home page-------------------
-var renderHomepage = function(req, res, responseBody) {
-	var message;
-	if (!(responseBody instanceof Array)) {
-		message = "API lookup error";
-		responseBody = [];
-	} else {
-		if (!responseBody.length) {
-			message = "No places found nearby";
-		}
-	}  
+var renderHomepage = function(req, res) {
 	res.render('locations_list', { 
 	  title: 'Loc8r - find a place to work with wifi',
 	  pageHeader:{
 		  title: 'Loc8r',
 		  strapline:'Find places to work with wifi near you!',
-  		locations : responseBody,
-  		message : message
-	  }
-  }); 
+	  },
+	  sidebar: "Looking for wifi and a seat? Loc8r helps you find places to \
+		  work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r \
+		  help you find the place you're looking for."
+	}); 
 };
 //--------ROUTE: router.get('/', ...homeList)------------
 module.exports.homeList = function(req, res){
-	var requestOptions, path;
-	path = '/api/locations';
-	requestOptions = {
-		url : apiOptions.server + path,
-		method : "GET",
-		json : {},
-		qs : {
-			lng : -0.969088,
-			lat : 51.4550405,
-			maxDistance : 20
-			}
-		};
-	request(requestOptions, function(err, response, body) {
-		var i, data;
-		data = body;
-		if (response.statusCode === 200 && data.length) {
-			for (i=0; i<data.length; i++) {
-				data[i].distance = _formatDistance(data[i].distance);
-				}
-		}
-		renderHomepage(req, res, data);
-	});
+		renderHomepage(req, res);
 };
 
 //-------------------render Location Details page-----------------
