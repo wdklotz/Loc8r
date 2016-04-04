e17dd871cf5c0204eeb80fe7e7b07b7a6361ae4a
Wolf-Dieter Klotz <wdklotz@gmail.com>
2016-04-04 23:33:17 +0200

    book pp 321

--- app_client/app.js
+++ app_client/app.js
@@ -1,12 +1,17 @@
 (function() {     // open IIFE
 
-  angular.module('loc8rApp', ['ngRoute']);
+  angular.module('loc8rApp', ['ngRoute','ngSanitize']);
 
   function config($routeProvider, $locationProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'home/home.view.html',
         controller: 'homeCtrl',
+        controllerAs: 'vm'
+      })
+      .when('/about', {
+        templateUrl: '/common/views/genericText.view.html',
+        controller: 'aboutCtrl',
         controllerAs: 'vm'
       })
       .otherwise({redirectTo: '/'});
