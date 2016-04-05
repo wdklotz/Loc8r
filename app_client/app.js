f7237884aa164404a40d773568dd140744922558
Wolf-Dieter Klotz <wdklotz@gmail.com>
2016-04-05 14:53:47 +0200

    book pp 327

--- app_client/app.js
+++ app_client/app.js
@@ -5,13 +5,18 @@
   function config($routeProvider, $locationProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'home/home.view.html',
+        templateUrl: '/home/home.view.html',
         controller: 'homeCtrl',
         controllerAs: 'vm'
       })
       .when('/about', {
         templateUrl: '/common/views/genericText.view.html',
         controller: 'aboutCtrl',
+        controllerAs: 'vm'
+      })
+      .when('/location/:locationid', {
+        templateUrl: '/locationDetail/locationDetail.view.html',
+        controller: 'locationDetailCtrl',
         controllerAs: 'vm'
       })
       .otherwise({redirectTo: '/'});
--- app_client/common/services/loc8rData.service.js
+++ app_client/common/services/loc8rData.service.js
@@ -7,12 +7,18 @@ function loc8rData      ($http, $log) {
       return $http.get('/api/locations?lng=' + lng + '&lat=' + lat + '&maxDistance=15000');
       // return $http.get('/api/locations?lng=5.672924999999964&lat=45.1199439&maxDistance=2000');
    };
+
+   var locationById = function(locationid) {
+     return $http.get('/api/location/' + locationid);
+   };
+
    return {
-      locationByCoords  : locationByCoords
+      locationByCoords  : locationByCoords,
+      locationById : locationById
    };
 };
 
 angular.module('loc8rApp')
-  .service('loc8rData',loc8rData);
+  .service('loc8rData', loc8rData);
 
 })();  // close & invoke IIFE
