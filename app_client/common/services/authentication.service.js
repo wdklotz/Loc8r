b8500bd5e6faab653b02b678017d669be68f5581
Wolf-Dieter Klotz <wdklotz@gmail.com>
2016-04-08 00:19:12 +0200

    book pp 377

--- app_client/common/services/authentication.service.js
+++ app_client/common/services/authentication.service.js
@@ -3,34 +3,60 @@
 angular.module('loc8rApp')
   .service('authentication', authentication);
 
-  authentication.$inject = ['$window','$http'];
-  function authentication   ($window,  $http) {
+authentication.$inject = ['$window','$http'];
+function authentication   ($window,  $http) {
 
-    var saveToken = function (token) {
-      $window.localStorage['loc8r-token'] = token;
-    };
+  var saveToken = function (token) {
+    $window.localStorage['loc8r-token'] = token;
+  };
 
-    var getToken = function () {
-      return $window.localStorage['loc8r-token'];
-    };
+  var getToken = function () {
+    return $window.localStorage['loc8r-token'];
+  };
 
-    var register = function(user) {
-      return $http.post('/api/register', user).success(function(data) {
-        saveToken(data.token);
-      });
-    };
+  var register = function(user) {
+    return $http.post('/api/register', user).success(function(data) {
+      saveToken(data.token);
+    });
+  };
 
-    var login = function(user) {
-      return $http.post('/api/login', user).success(function(data) {
-        saveToken(data.token);
-      });
-    };
+  var login = function(user) {
+    return $http.post('/api/login', user).success(function(data) {
+      saveToken(data.token);
+    });
+  };
 
-    var logout = function() {
-      $window.localStorage.removeItem('loc8r-token');
-    };
+  var logout = function() {
+    $window.localStorage.removeItem('loc8r-token');
+  };
 
-  return { saveToken: saveToken, getToken: getToken, register: register, login: login, logout: logout };
-  }
+  var isLoggedIn = function() {
+    var token = getToken();
+    if(token) {
+      var payload = JSON.parse($window.atob(token.split('.')[1]));
+      return payload.exp > Date.now() / 1000;
+    } else {
+      return false;
+    }
+  };
+
+  var currentUser = function() {
+    if(isLoggedIn()) {
+      var token = getTocken();
+      var payload = JSON.parse($window.atob(token.split('.')[1]));
+      return {email: payload.email, name: payload.name};
+    }
+  };
+
+  return {
+    saveToken: saveToken,
+    getToken: getToken,
+    register: register,
+    login: login,
+    logout: logout,
+    isLoggedIn: isLoggedIn,
+    currentUser: currentUser
+  };
+}
 
 })();
